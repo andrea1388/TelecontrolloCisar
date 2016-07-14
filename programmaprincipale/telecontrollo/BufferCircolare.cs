a180cd828c4a228263a60343b80f7b60f1c40cae
andrea1388 <andrea.carrara@libero.it>
2016-07-14 14:40:01 +0200

    Update BufferCircolare.cs

--- programmaprincipale/telecontrollo/BufferCircolare.cs
+++ programmaprincipale/telecontrollo/BufferCircolare.cs
@@ -10,16 +10,21 @@ namespace telecontrollo
         byte lunghezzabuffer,lunghezzacodice;
         private int p1;
         private int p2;
+        byte[] array;
         public BufferCircolare(byte lunghezzabuffer, byte lunghezzacodice)
         {
             this.lunghezzabuffer = lunghezzabuffer;
             this.lunghezzacodice = lunghezzacodice;
             if (lunghezzacodice > lunghezzabuffer) throw new Exception("Buffer insufficiente");
+            array=new byte[lunghezzabuffer];
         }
 
        
-        public void Inserisci(byte tono);
-        String LeggiCodice()
+        public void Inserisci(byte tono)
+        {
+            
+        }
+        String EstraiUltimiDati(byte quantita)
         {
 
         }
