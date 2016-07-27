201f7915610fba8be14efd69ce5038c3668b75e2
Andrea Carrara <andrea.carrara@libero.it>
2016-07-27 23:07:19 +0200

    init controllolinea

---
+++ programmaprincipale/controllolinea/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+// controllolinea <numero linea> <on|off>
+namespace telecontrollo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("controllolinea By Andrea Carrara 2016");
+            if(args.Length!=2)
+            {
+                Console.WriteLine("Uso: controllolinea <numero linea> <on|off>");
+            }
+            try
+            {
+                IniParser parser = new IniParser("telecontrollo.conf");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Problemi con il file di configurazione\n" +ex.Message );
+                return;
+            }
+
+        }
+    }
+}
--- programmaprincipale/telecontrollo/Program.cs
+++ programmaprincipale/telecontrollo/Program.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿//#define Debug
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
 using System.Threading;
+using Raspberry.IO.Components.Expanders.Pcf8574;
 using Raspberry.IO.GeneralPurpose;
+using Raspberry.IO.InterIntegratedCircuit;
 
 /*
     Attende comandi dtmf
@@ -11,6 +14,7 @@ using Raspberry.IO.GeneralPurpose;
     <codice di sblocco><comando: 0 | 1><linea da 00 a 99 sempre su due cifre>
     0 spegne 1 accende
 */
+
 
 namespace telecontrollo
 {
@@ -36,19 +40,19 @@ namespace telecontrollo
     {
         BufferCircolare buffer;
         Stopwatch duratacomandodtmf = new Stopwatch();
-        int tempomassimoduratacomandodtmf = 15000; // tempo max entro il quale completare il comandoi dtmf
+        int tempomassimoduratacomandodtmf = 150000; // tempo max entro il quale completare il comandoi dtmf
         byte stato = 0;
         char comando;
         String linea;
         CodiceDtmf codicedtmf;
         ProcessorPins pins, lettura;
         IGpioConnectionDriver  driver;
-        
+        byte[] indirizzi_pcf; // indirizzi i2c dei pcf
+        I2cDriver i2cdriver;
+        const ProcessorPin pinAggangioLinea=ProcessorPin.Pin10;
+
         public void mainloop(String path2conffile)
         {
-            log("Server telecontrollo partito");
-            log("By iw3gcb - luglio 2016");
-            this.pins = ProcessorPins.Pin17 | ProcessorPins.Pin27 | ProcessorPins.Pin22 | ProcessorPins.Pin23 | ProcessorPins.Pin24 | ProcessorPins.Pin25;
 
             IniParser parser = new IniParser(path2conffile);
             bool tonodisponibile, squillotelefono;
@@ -58,45 +62,46 @@ namespace telecontrollo
             int tempominimoduratasquillo = 250; // tempo minimo durata dello squillo prima di considerarlo valido, in ms
             bool lineaconnessa = false; // indica se la linea è agganciata
             int tempomassimochiamata = 30000; // tempo max durata telefonata
-            byte[] indirizzi_pcf; // indirizzi i2c dei pcf
-
+            const ConnectorPin sdaPin = ConnectorPin.P1Pin03;
+            const ConnectorPin sclPin = ConnectorPin.P1Pin05;
+           
             if(!int.TryParse(parser.GetSetting("ROOT", "intervallopolling"),out intervallopolling)) intervallopolling=10;
             String tmp= parser.GetSetting("ROOT", "codicedtmf");
             if(tmp==null) tmp = "1968*";
             codicedtmf=new CodiceDtmf(tmp);
             buffer = new BufferCircolare((byte)(codicedtmf.Length+10));
             //log("codicedtmf=" + codicedtmf.ToString() );
+            tmp = parser.GetSetting("ROOT", "indirizziPcf");
+            String[] indirizzi = tmp.Split(',');
+            indirizzi_pcf=new byte[indirizzi.Length ];
+            for (int ip = 0; ip < indirizzi.Length; ip++) indirizzi_pcf[ip] = Convert.ToByte (indirizzi[ip],16);
+
+            log("Server telecontrollo partito");
+            log("By iw3gcb - luglio 2016");
             log("intervallopolling=" + intervallopolling);
             Stopwatch tempochiamata = new Stopwatch();
             
             // definizione pin i/o
-            const ConnectorPin led1Pin = ConnectorPin.P1Pin26;
-            const ProcessorPin pin1=ProcessorPin.Pin29;
-            const ProcessorPin pin2=ProcessorPin.Pin2;
-
-            var pinTonoDisponibile = ProcessorPin.Pin17;
-            pinTonoDisponibile.Input();
-            var pinSquillo = ConnectorPin.P1Pin22.ToProcessor();
-            var pinAggangioLinea = ConnectorPin.P1Pin19.ToProcessor();
-            const pinTono0 = ConnectorPin.P1Pin13.ToProcessor();
-            var pinTono1 = ConnectorPin.P1Pin15.ToProcessor();
-            var pinTono2 = ConnectorPin.P1Pin16.ToProcessor();
-            var pinTono3 = ConnectorPin.P1Pin18.ToProcessor();
-             driver = GpioConnectionSettings.DefaultDriver;
+            // pin da leggere con una sola lettura contemporanea
+            // 17= tonodisponibile, 21=bit0 tono, 22=bit1, 23= bit3, 24=bit3, 25=squillo
+            #if !Debug
+            i2cdriver = new I2cDriver(sdaPin.ToProcessor(), sclPin.ToProcessor());
+            pins = ProcessorPins.Pin17 | ProcessorPins.Pin21 | ProcessorPins.Pin22 | ProcessorPins.Pin23 | ProcessorPins.Pin24 | ProcessorPins.Pin25;
+            driver = GpioConnectionSettings.DefaultDriver;
             driver.Allocate(pinAggangioLinea, PinDirection.Output);
-            driver.Allocate(pinTonoDisponibile, PinDirection.Input);
-            driver.Allocate(pinSquillo, PinDirection.Input);
-            driver.Allocate(pinTono0, PinDirection.Input);
-            driver.Allocate(pinTono1, PinDirection.Input);
-            driver.Allocate(pinTono2, PinDirection.Input);
-            driver.Allocate(pinTono3, PinDirection.Input);
- 
+            driver.Allocate(ProcessorPin.Pin17, PinDirection.Input);
+            driver.Allocate(ProcessorPin.Pin21, PinDirection.Input);
+            driver.Allocate(ProcessorPin.Pin22, PinDirection.Input);
+            driver.Allocate(ProcessorPin.Pin23, PinDirection.Input);
+            driver.Allocate(ProcessorPin.Pin24, PinDirection.Input);
+            driver.Allocate(ProcessorPin.Pin25, PinDirection.Input);
+#endif
             while (true)
             {
                 leggiingressi(out tonodisponibile, out squillotelefono, out tono);
                 if (tonodisponibile)
                 {
-                    //log("ricevuto tono: " + tono.Valore);
+                    log("ricevuto tono: " + tono.Valore);
                     ElaboraSequenzaToniRicevuti(tono);
 
 
@@ -131,20 +136,36 @@ namespace telecontrollo
         }
         void leggiingressi(out bool tonodisponibile, out bool squillotelefono, out TonoDtmf tono)
         {
+            #if !Debug
             lettura = driver.Read(pins);
-            Random rnd = new Random();
-            tono = new TonoDtmf((byte)rnd.Next(16) );
             tonodisponibile = (lettura & ProcessorPins.Pin17)>0;
             squillotelefono = (lettura & ProcessorPins.Pin17)>0;
-
+            byte t;
+            t = (byte)(((uint)lettura >> 21) & 0x0f);
+            tono = new TonoDtmf(t);
+            #else
+            Random rnd = new Random();
+            uint lettura = (uint)rnd.Next(0xfffffff);
+            //tono = new TonoDtmf((byte)rnd.Next(16));
+            byte t;
+            t = (byte)(((uint)lettura >> 21) & 0x0f);
+            tono = new TonoDtmf(t);
+            tonodisponibile = true;
+            squillotelefono = false;
+#endif
 
         }
         void AgganciaLineaTelefonica()
         {
-
+            #if !Debug
+            driver.Write(pinAggangioLinea, true);
+            #endif
         }
         void SganciaLineaTelefonica()
         {
+            #if !Debug
+            driver.Write(pinAggangioLinea, false);
+            #endif
 
         }
         void ElaboraSequenzaToniRicevuti(TonoDtmf tono)
@@ -166,7 +187,7 @@ namespace telecontrollo
                     comando=tono.Carattere;
                     if(comando!='0' && comando !='1')
                     {
-                        log("comando errato" + comando);
+                        log("comando errato: " + comando);
                         stato = 0;
                     }
                     else
@@ -193,16 +214,27 @@ namespace telecontrollo
         }
         void ComandoRicevuto(char comando,String linea)
         {
-            byte indirizzo_pcf=indirizzi_pcf[linea / 8];
+            int l;
+            if(!int.TryParse(linea,out l))
+            {
+                log("ComandoRicevuto:Numero linea errata:" + linea);
+                return;
+            }
+            byte indirizzo_pcf=indirizzi_pcf[l / 8];
+            Pcf8574Pin bitdacontrollare = (Pcf8574Pin)(Math.Pow (2, (l % 8)));
+            #if !Debug
+            var deviceConnection = new Pcf8574I2cConnection(i2cdriver.Connect(indirizzo_pcf));
             switch (comando)
             {
                 case '0': //spegni
-                    
+                    deviceConnection.SetPinStatus(bitdacontrollare , false);
                     break;
                 case '1': //accendi
+                    deviceConnection.SetPinStatus(bitdacontrollare, true);
                     break;
                 
             }
+            #endif
         }
       
         static void log(String msg)
