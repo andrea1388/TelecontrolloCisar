3d1a97759bea166e4bb4e5c64a04f78d4d22b264
Andrea Carrara <andrea.carrara@libero.it>
2016-07-28 08:41:59 +0200

    mod

--- programmaprincipale/controllolinea/Program.cs
+++ programmaprincipale/controllolinea/Program.cs
@@ -25,6 +25,23 @@ namespace telecontrollo
                 Console.WriteLine("Problemi con il file di configurazione\n" +ex.Message );
                 return;
             }
+            String tmp = parser.GetSetting("ROOT", "indirizziPcf");
+            String[] indirizzi = tmp.Split(',');
+            indirizzi_pcf=new byte[indirizzi.Length ];
+            for (int ip = 0; ip < indirizzi.Length; ip++) indirizzi_pcf[ip] = Convert.ToByte (indirizzi[ip],16);
+            int linea;
+            if(!int.TryParse(args[1],out linea))
+            {
+                log("linea errata");
+                return;
+            }
+            if(linea<0 || linea>indirizzi_pcf.Length *8)
+            {
+                log("linea errata");
+                return;
+            }
+            if (args[2]!=)
+}
 
         }
     }
