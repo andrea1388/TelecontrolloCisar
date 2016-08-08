fbdf8db0e1f6d22e23d0ac9d683d89ca3b036ea8
Andrea Carrara <andrea.carrara@libero.it>
2016-08-08 12:35:07 +0200

    hwhost

---
+++ programmaprincipale/SchedaHWHost/ISchedaHW.cs
@@ -0,0 +1,7 @@
+﻿using System.ServiceModel;
+[ServiceContract]
+public interface ISchedaHW
+{
+    [OperationContract]
+    bool ImpostaLinea(byte numero, bool stato);
+}
---
+++ programmaprincipale/SchedaHWHost/SchedaHW.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SchedaHWHost
+{
+    class SchedaHW : ISchedaHW
+    {
+        public bool ImpostaLinea(byte numero, bool stato)
+        {
+            return true;
+        }
+    }
+}
