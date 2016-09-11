ac99d78c2af2dc29bbff983a46a5d8d6b893d21e
Andrea Carrara <andrea.carrara@libero.it>
2016-09-11 11:36:21 +0200

    web part2

---
+++ programmaprincipale/TelecontrolloWebApp/Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="telecontrollo.Default" %>
+<!DOCTYPE html>
+<html lang="it" xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+    <title>Telecontrollo Web App</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet"/>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="container">
+    
+        <div id="jj" runat="server">
+        </div>
+        <asp:button runat="server" text="Buttone" class="btn btn-info" ID="bt1" OnClick="btn1_Click"/>
+    </div>
+    </form>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ programmaprincipale/TelecontrolloWebApp/Default.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace telecontrollo
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            MostraDispositivi();            
+            if(IsPostBack )
+            {
+            }
+            
+
+        }
+        void MostraDispositivi()
+        {
+            int numDev = Global.scheda.NumeroLineeIO;
+            String j="";
+            for (int i = 0; i < numDev; i++)
+            {
+                j += "<div id=\"linea" + i.ToString() + "\" class=\"col-md-1\">\n";
+                j += "Linea " + i.ToString() + "\n";
+                j += "<button id=\"btn" + i.ToString() + "\" type=\"submit\" class=\"btn btn-success\" runat=\"server\">Success</button>";
+                j += "</div>\n";
+            }
+            jj.InnerHtml=j;
+        }
+
+        protected void bt1_Click(object sender, EventArgs e)
+        {
+
+        }
+        protected void btn1_Click(object sender, EventArgs e)
+        {
+
+        }
+       
+
+       
+    }
+}
